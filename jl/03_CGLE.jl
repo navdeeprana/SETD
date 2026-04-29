9558aaacb06d2e7881fe209560903917f9203527
navdeep rana <navdeepr@tifrh.res.in>
2026-04-29 12:37:20 +0200

    runic

--- jl/03_CGLE.jl
+++ jl/03_CGLE.jl
@@ -6,7 +6,7 @@
 #       extension: .jl
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.17.3
+#       jupytext_version: 1.19.1
 #   kernelspec:
 #     display_name: Julia 1.10
 #     language: julia
@@ -33,12 +33,12 @@ set_theme!(makietheme())
 # %%
 # Define parameters
 nx, lx = 1024, 320π
-tmax, h, saveat, save_after = 1.e+3, 1.e-1, 1.e+1, 0.e+0
+tmax, h, saveat, save_after = 1.0e+3, 1.0e-1, 1.0e+1, 0.0e+0
 seed = 42
-pars = (b = 0.0, c = 0.5, D = 1.e-2, q = 16 * (2π / lx));
+pars = (b = 0.0, c = 0.5, D = 1.0e-2, q = 16 * (2π / lx));
 
 # %%
-fout = @sprintf "data/scratch_nx_%04d_ln_%04d.jld2" nx round(Int, lx/(2π))
+fout = @sprintf "data/scratch_nx_%04d_ln_%04d.jld2" nx round(Int, lx / (2π))
 sim = cgle_simulation(nx, lx, pars, tmax, h, saveat, save_after, seed, fout);
 
 # %%
@@ -60,7 +60,7 @@ fig
 # %%
 # Analysis
 nx, lx = 1024, 320π
-tmax, h, saveat, save_after = 2.e+5, 2.e-2, 2.e+2, 0.0
+tmax, h, saveat, save_after = 2.0e+5, 2.0e-2, 2.0e+2, 0.0
 pars = (b = 0.0, c = 0.1, q = 16 * (2π / lx));
 
 grid = create_grid(nx, lx)
@@ -76,18 +76,18 @@ end
 # %%
 fig, axes = figax(nx = 2, yscale = log10, s = 120, xlabel = L"k", ylabel = L"C_{\text{ET}}(k)")
 
-axes[1].limits = (-1, 1, 5.e-3, 1.e+3)
+axes[1].limits = (-1, 1, 5.0e-3, 1.0e+3)
 axes[1].xscale = Makie.pseudolog10
 
 ax = axes[2]
-ax.limits = (1.5π/grid.lx, 1.8, 1.e-2, 5.e+2)
+ax.limits = (1.5π / grid.lx, 1.8, 1.0e-2, 5.0e+2)
 ax.xscale = log10
 
 labels = [L"D=0.01", L"D=0.05", L"D=0.10"]
-for (D, m, c, l) in zip([0.01, 0.05, 0.10], [:circle, :rect, :diamond], colors, labels)
-    fdata = @sprintf "data/cgleD_nx_%04d_ln_%04d_D_%.2f.jld2" nx round(Int, lx/(2π)) D
-    sol = loaddata(fdata);
-    k, Ck = equal_time_correlator(grid, vars, sol; skip = 500);
+for (D, m, c, l) in zip([0.01, 0.05, 0.1], [:circle, :rect, :diamond], colors, labels)
+    fdata = @sprintf "data/cgleD_nx_%04d_ln_%04d_D_%.2f.jld2" nx round(Int, lx / (2π)) D
+    sol = loaddata(fdata)
+    k, Ck = equal_time_correlator(grid, vars, sol; skip = 500)
     Ck_an = Cet(k, pars.q, D, pars.c)
 
     if D == 0.01
@@ -104,8 +104,8 @@ for (D, m, c, l) in zip([0.01, 0.05, 0.10], [:circle, :rect, :diamond], colors, 
     scatterlines!(ax, kp, Ckp, marker = m, linewidth = 3, markersize = 20, label = l)
 end
 k = 0.02:0.1:1.0
-lines!(k, (@. 1.e-1/k^2), color = :black)
-text!(ax, L"$k^{-2}$"; position = (1.5e-1, 1.e+1), fontsize = 30)
+lines!(k, (@. 1.0e-1 / k^2), color = :black)
+text!(ax, L"$k^{-2}$"; position = (1.5e-1, 1.0e+1), fontsize = 30)
 axislegend.(axes, position = :rt)
 
 texts = abc(axes)
